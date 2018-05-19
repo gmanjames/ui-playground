362690daa1eb6efe253d7a8bb894083d7d8b082d
gmanjames <yms1465@gmail.com>
2018-05-19 10:08:40 -0500

    Add checks for page breakpoints on 'resize'.
    A few css rule changes made their way in...

--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,25 @@
 
 const Page = function()
 {
+    // Element to append gallery cards to
     const domElem = document.getElementById('gallery');
+
+    // Cheat and use bootstrap's breakpoints...
+    const BP = [
+
+         576,
+         768,
+         992,
+        1200
+    ];
+
+    // Boolean to track if a breakpoint has just been traversed
+    let __justPassed;
 
 
     const setup = () =>
     {
-
-
+        __justPassed = false;
     };
 
 
@@ -31,8 +43,39 @@ const Page = function()
         domElem.appendChild(card);
     };
 
+
+    const justPassed = (bp) =>
+    {
+        let delta = Math.abs(window.innerWidth - bp);
+        if (delta < 5 && !__justPassed) {
+            __justPassed = true;
+        } else {
+            __justPassed = false;
+        }
+
+        return __justPassed;
+    };
+
+
+    const getBreakpoint = () =>
+    {
+        let target;
+        for (let size in BP)
+        {
+            let bp = BP[size];
+            if (window.innerWidth >= bp) {
+                target = bp;
+            }
+        }
+
+        return target;
+    };
+
+
     return {
         setup,
-        addGalleryItem
+        addGalleryItem,
+        getBreakpoint,
+        justPassed
     };
 };
